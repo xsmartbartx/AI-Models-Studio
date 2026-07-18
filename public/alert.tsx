161b2b442cbb86e74bd5a33047c4367cb8776b1a
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-18 12:47:47 +0200

    alert title

--- public/alert.tsx
+++ public/alert.tsx
@@ -26,3 +26,14 @@ const Alert = React.forwardRef<
   <div ref={ref} role="alert" className={cn(alertVariants({ variant }), className)} {...props} />
 ));
 Alert.displayName = "Alert";
+
+const AlertTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
+  ({ className, ...props }, ref) => (
+    <h5
+      ref={ref}
+      className={cn("mb-1 font-medium leading-none tracking-tight", className)}
+      {...props}
+    />
+  ),
+);
+AlertTitle.displayName = "AlertTitle";
