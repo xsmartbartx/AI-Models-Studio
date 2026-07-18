62d1ec7ae1313125a7934f1b00aaa80a21a0b2c3
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-18 11:07:47 +0200

    dialog header

--- src/components/alert-dialog.tsx
+++ src/components/alert-dialog.tsx
@@ -42,3 +42,8 @@ const AlertDialogContent = React.forwardRef<
   </AlertDialogPortal>
 ));
 AlertDialogContent.displayName = AlertDialogPrimitive.Content.displayName;
+
+const AlertDialogHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={cn("flex flex-col space-y-2 text-center sm:text-left", className)} {...props} />
+);
+AlertDialogHeader.displayName = "AlertDialogHeader";
