f3c303a50bfbc75ac7334f2bfb5a7bfb47bcc85f
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-18 11:08:06 +0200

    dialog footer

--- src/components/alert-dialog.tsx
+++ src/components/alert-dialog.tsx
@@ -47,3 +47,11 @@ const AlertDialogHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDiv
   <div className={cn("flex flex-col space-y-2 text-center sm:text-left", className)} {...props} />
 );
 AlertDialogHeader.displayName = "AlertDialogHeader";
+
+const AlertDialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn("flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2", className)}
+    {...props}
+  />
+);
+AlertDialogFooter.displayName = "AlertDialogFooter";
