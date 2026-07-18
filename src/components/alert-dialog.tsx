205ff8a75962bf5c1f153b209e23c4f3fe879ff1
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-18 11:08:56 +0200

    dialog description

--- src/components/alert-dialog.tsx
+++ src/components/alert-dialog.tsx
@@ -67,3 +67,15 @@ const AlertDialogTitle = React.forwardRef<
   />
 ));
 AlertDialogTitle.displayName = AlertDialogPrimitive.Title.displayName;
+
+const AlertDialogDescription = React.forwardRef<
+  React.ElementRef<typeof AlertDialogPrimitive.Description>,
+  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Description>
+>(({ className, ...props }, ref) => (
+  <AlertDialogPrimitive.Description
+    ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    {...props}
+  />
+));
+AlertDialogDescription.displayName = AlertDialogPrimitive.Description.displayName;
