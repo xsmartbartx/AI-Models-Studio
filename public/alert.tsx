97694d7fe33dbe56a1b1c6c544d82b230a61e49a
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-18 12:48:07 +0200

    alert decription

--- public/alert.tsx
+++ public/alert.tsx
@@ -37,3 +37,13 @@ const AlertTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<H
   ),
 );
 AlertTitle.displayName = "AlertTitle";
+
+const AlertDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("text-sm [&_p]:leading-relaxed", className)} {...props} />
+));
+AlertDescription.displayName = "AlertDescription";
+
+export { Alert, AlertTitle, AlertDescription };
