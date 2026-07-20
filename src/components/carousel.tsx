7948d60a10f5e12357b1ef1083191313c1f38cc3
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-20 20:10:00 +0200

    carousel content

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -131,3 +131,24 @@ const Carousel = React.forwardRef<
   );
 });
 Carousel.displayName = "Carousel";
+
+const CarouselContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => {
+    const { carouselRef, orientation } = useCarousel();
+
+    return (
+      <div ref={carouselRef} className="overflow-hidden">
+        <div
+          ref={ref}
+          className={cn(
+            "flex",
+            orientation === "horizontal" ? "-ml-4" : "-mt-4 flex-col",
+            className,
+          )}
+          {...props}
+        />
+      </div>
+    );
+  },
+);
+CarouselContent.displayName = "CarouselContent";
