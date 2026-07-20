cf59bca60830f05bb8dadf2e778d131493b0aa41
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-20 19:16:50 +0200

    breadcrumb

---
+++ src/components/breadcrumb.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { ChevronRight, MoreHorizontal } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+
+const Breadcrumb = React.forwardRef<
+  HTMLElement,
+  React.ComponentPropsWithoutRef<"nav"> & {
+    separator?: React.ReactNode;
+  }
+>(({ ...props }, ref) => <nav ref={ref} aria-label="breadcrumb" {...props} />);
+Breadcrumb.displayName = "Breadcrumb";
