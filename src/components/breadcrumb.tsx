8f0bb39a31240ada4ae8a51d2af7e8e40683f3e6
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-20 19:24:02 +0200

    breadcrumb list

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -11,3 +11,17 @@ const Breadcrumb = React.forwardRef<
   }
 >(({ ...props }, ref) => <nav ref={ref} aria-label="breadcrumb" {...props} />);
 Breadcrumb.displayName = "Breadcrumb";
+
+const BreadcrumbList = React.forwardRef<HTMLOListElement, React.ComponentPropsWithoutRef<"ol">>(
+  ({ className, ...props }, ref) => (
+    <ol
+      ref={ref}
+      className={cn(
+        "flex flex-wrap items-center gap-1.5 break-words text-sm text-muted-foreground sm:gap-2.5",
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
+BreadcrumbList.displayName = "BreadcrumbList";
