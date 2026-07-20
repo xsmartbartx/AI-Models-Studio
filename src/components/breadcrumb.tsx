4d52fd1ac616c46332afbbbd0893df5afd17ed6c
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-20 19:29:34 +0200

    ellipsis

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -76,3 +76,26 @@ const BreadcrumbSeparator = ({ children, className, ...props }: React.ComponentP
   </li>
 );
 BreadcrumbSeparator.displayName = "BreadcrumbSeparator";
+
+const BreadcrumbEllipsis = ({ className, ...props }: React.ComponentProps<"span">) => (
+  <span
+    role="presentation"
+    aria-hidden="true"
+    className={cn("flex h-9 w-9 items-center justify-center", className)}
+    {...props}
+  >
+    <MoreHorizontal className="h-4 w-4" />
+    <span className="sr-only">More</span>
+  </span>
+);
+BreadcrumbEllipsis.displayName = "BreadcrumbElipssis";
+
+export {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+  BreadcrumbEllipsis,
+};
