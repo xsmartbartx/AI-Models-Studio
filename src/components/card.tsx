9a963e28088dc85e1042685afe50a7e2ff7d880f
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-20 20:02:07 +0200

    card contenet & footer

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -37,3 +37,19 @@ const CardDescription = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HT
   ),
 );
 CardDescription.displayName = "CardDescription";
+
+const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  ),
+);
+CardContent.displayName = "CardContent";
+
+const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("flex items-center p-6 pt-0", className)} {...props} />
+  ),
+);
+CardFooter.displayName = "CardFooter";
+
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent };
