c9ee36033e2dbee64c9ae371a2bb50b3b7c075d1
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-21 22:26:34 +0200

    CommandShortcut

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -119,3 +119,25 @@ const CommandItem = React.forwardRef<
 ));
 
 CommandItem.displayName = CommandPrimitive.Item.displayName;
+
+const CommandShortcut = ({ className, ...props }: React.HTMLAttributes<HTMLSpanElement>) => {
+  return (
+    <span
+      className={cn("ml-auto text-xs tracking-widest text-muted-foreground", className)}
+      {...props}
+    />
+  );
+};
+CommandShortcut.displayName = "CommandShortcut";
+
+export {
+  Command,
+  CommandDialog,
+  CommandInput,
+  CommandList,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandShortcut,
+  CommandSeparator,
+};
