22685715f2c29e916374689480861930c5e6df69
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-21 22:25:56 +0200

    CommandSeparator

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -91,3 +91,15 @@ const CommandGroup = React.forwardRef<
 ));
 
 CommandGroup.displayName = CommandPrimitive.Group.displayName;
+
+const CommandSeparator = React.forwardRef<
+  React.ElementRef<typeof CommandPrimitive.Separator>,
+  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Separator>
+>(({ className, ...props }, ref) => (
+  <CommandPrimitive.Separator
+    ref={ref}
+    className={cn("-mx-1 h-px bg-border", className)}
+    {...props}
+  />
+));
+CommandSeparator.displayName = CommandPrimitive.Separator.displayName;
