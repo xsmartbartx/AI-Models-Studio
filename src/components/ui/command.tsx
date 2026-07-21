b0c89a7a7dc49f7da9c016060e05c9ba59e962c5
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-21 22:25:40 +0200

    CommandGroup

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -75,3 +75,19 @@ const CommandEmpty = React.forwardRef<
 ));
 
 CommandEmpty.displayName = CommandPrimitive.Empty.displayName;
+
+const CommandGroup = React.forwardRef<
+  React.ElementRef<typeof CommandPrimitive.Group>,
+  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Group>
+>(({ className, ...props }, ref) => (
+  <CommandPrimitive.Group
+    ref={ref}
+    className={cn(
+      "overflow-hidden p-1 text-foreground [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground",
+      className,
+    )}
+    {...props}
+  />
+));
+
+CommandGroup.displayName = CommandPrimitive.Group.displayName;
