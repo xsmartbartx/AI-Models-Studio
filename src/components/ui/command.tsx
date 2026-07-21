3b5e428fc3e3c5dd70fb83e8713bfbf683268b9d
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-21 22:25:04 +0200

    CommandList

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -53,3 +53,16 @@ const CommandInput = React.forwardRef<
 ));
 
 CommandInput.displayName = CommandPrimitive.Input.displayName;
+
+const CommandList = React.forwardRef<
+  React.ElementRef<typeof CommandPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof CommandPrimitive.List>
+>(({ className, ...props }, ref) => (
+  <CommandPrimitive.List
+    ref={ref}
+    className={cn("max-h-[300px] overflow-y-auto overflow-x-hidden", className)}
+    {...props}
+  />
+));
+
+CommandList.displayName = CommandPrimitive.List.displayName;
