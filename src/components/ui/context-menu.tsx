4eced6ed415d4291414730fa9d059e6acf288f2e
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-23 12:59:07 +0200

    menu label

--- src/components/ui/context-menu.tsx
+++ src/components/ui/context-menu.tsx
@@ -131,3 +131,17 @@ const ContextMenuRadioItem = React.forwardRef<
   </ContextMenuPrimitive.RadioItem>
 ));
 ContextMenuRadioItem.displayName = ContextMenuPrimitive.RadioItem.displayName;
+
+const ContextMenuLabel = React.forwardRef<
+  React.ElementRef<typeof ContextMenuPrimitive.Label>,
+  React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.Label> & {
+    inset?: boolean;
+  }
+>(({ className, inset, ...props }, ref) => (
+  <ContextMenuPrimitive.Label
+    ref={ref}
+    className={cn("px-2 py-1.5 text-sm font-semibold text-foreground", inset && "pl-8", className)}
+    {...props}
+  />
+));
+ContextMenuLabel.displayName = ContextMenuPrimitive.Label.displayName;
