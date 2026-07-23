ba5079584c8c8534a61616fd879b7f38833a0bc7
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-23 13:05:17 +0200

    dialog header & footer

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -52,3 +52,16 @@ const DialogContent = React.forwardRef<
   </DialogPortal>
 ));
 DialogContent.displayName = DialogPrimitive.Content.displayName;
+
+const DialogHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={cn("flex flex-col space-y-1.5 text-center sm:text-left", className)} {...props} />
+);
+DialogHeader.displayName = "DialogHeader";
+
+const DialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn("flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2", className)}
+    {...props}
+  />
+);
+DialogFooter.displayName = "DialogFooter";
