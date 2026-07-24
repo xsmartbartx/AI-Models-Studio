237df4b1459bedc0d5e8e01668b39f5aa188a746
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-24 20:01:00 +0200

    form description

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -116,3 +116,20 @@ const FormControl = React.forwardRef<
   );
 });
 FormControl.displayName = "FormControl";
+
+const FormDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => {
+  const { formDescriptionId } = useFormField();
+
+  return (
+    <p
+      ref={ref}
+      id={formDescriptionId}
+      className={cn("text-[0.8rem] text-muted-foreground", className)}
+      {...props}
+    />
+  );
+});
+FormDescription.displayName = "FormDescription";
