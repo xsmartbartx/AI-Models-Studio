2a5965c465451008686b1a85975e7c1552b23674
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-24 19:49:52 +0200

    drawer header & footer

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -49,3 +49,13 @@ const DrawerContent = React.forwardRef<
   </DrawerPortal>
 ));
 DrawerContent.displayName = "DrawerContent";
+
+const DrawerHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={cn("grid gap-1.5 p-4 text-center sm:text-left", className)} {...props} />
+);
+DrawerHeader.displayName = "DrawerHeader";
+
+const DrawerFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={cn("mt-auto flex flex-col gap-2 p-4", className)} {...props} />
+);
+DrawerFooter.displayName = "DrawerFooter";
