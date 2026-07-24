cfa3398984e053dfc21d910166008d9dcba61f66
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-24 19:53:04 +0200

    form field usage

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -35,3 +35,34 @@ const FormField = <
     </FormFieldContext.Provider>
   );
 };
+
+const useFormField = () => {
+  const fieldContext = React.useContext(FormFieldContext);
+  const itemContext = React.useContext(FormItemContext);
+  const { getFieldState, formState } = useFormContext();
+
+  if (!fieldContext) {
+    throw new Error("useFormField should be used within <FormField>");
+  }
+
+  if (!itemContext) {
+    throw new Error("useFormField should be used within <FormItem>");
+  }
+
+  const fieldState = getFieldState(fieldContext.name, formState);
+
+  const { id } = itemContext;
+
+  return {
+    id,
+    name: fieldContext.name,
+    formItemId: `${id}-form-item`,
+    formDescriptionId: `${id}-form-item-description`,
+    formMessageId: `${id}-form-item-message`,
+    ...fieldState,
+  };
+};
+
+type FormItemContextValue = {
+  id: string;
+};
