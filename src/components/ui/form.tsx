6ad4d9350d547d7801a632b3da55a4d5c4c69ff7
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-24 20:02:11 +0200

    form message

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -133,3 +133,38 @@ const FormDescription = React.forwardRef<
   );
 });
 FormDescription.displayName = "FormDescription";
+
+const FormMessage = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, children, ...props }, ref) => {
+  const { error, formMessageId } = useFormField();
+  const body = error ? String(error?.message ?? "") : children;
+
+  if (!body) {
+    return null;
+  }
+
+  return (
+    <p
+      ref={ref}
+      id={formMessageId}
+      className={cn("text-[0.8rem] font-medium text-destructive", className)}
+      {...props}
+    >
+      {body}
+    </p>
+  );
+});
+FormMessage.displayName = "FormMessage";
+
+export {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+};
