49f29c6c5c5d7023f7539d15277fa57f63868cfc
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-07-24 20:00:20 +0200

    form control

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -98,3 +98,21 @@ const FormLabel = React.forwardRef<
   );
 });
 FormLabel.displayName = "FormLabel";
+
+const FormControl = React.forwardRef<
+  React.ElementRef<typeof Slot>,
+  React.ComponentPropsWithoutRef<typeof Slot>
+>(({ ...props }, ref) => {
+  const { error, formItemId, formDescriptionId, formMessageId } = useFormField();
+
+  return (
+    <Slot
+      ref={ref}
+      id={formItemId}
+      aria-describedby={!error ? `${formDescriptionId}` : `${formDescriptionId} ${formMessageId}`}
+      aria-invalid={!!error}
+      {...props}
+    />
+  );
+});
+FormControl.displayName = "FormControl";
